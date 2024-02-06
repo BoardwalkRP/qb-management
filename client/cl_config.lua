9b87fa9109a3d6331a4c8576cd0bdce866c8cbb6
citRa <citrahop@protonmail.com>
2024-02-06 19:12:59 -0400

    Cleanup

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -115,21 +115,6 @@ Config.BossMenuZones = {
 }
 
 Config.GangMenus = {
-    ['lostmc'] = {
-        vector3(0, 0, 0),
-    },
-    ['ballas'] = {
-        vector3(0, 0, 0),
-    },
-    ['vagos'] = {
-        vector3(0, 0, 0),
-    },
-    ['cartel'] = {
-        vector3(0, 0, 0),
-    },
-    ['families'] = {
-        vector3(0, 0, 0),
-    },
     ['tortellini'] = {
         vector3(425.06, -1512.18, 33.81),
     },
